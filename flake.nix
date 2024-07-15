b356b021389772230120ebf77caf69f8138015c9
Joshua Peek <josh@users.noreply.github.com>
2024-07-15 21:46:25 +0000

    Remove comments

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of codespace";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -19,12 +18,7 @@
         home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
 
-          # Specify your home configuration modules here, for example,
-          # the path to your home.nix.
           modules = [ ./home.nix ];
-
-          # Optionally use extraSpecialArgs
-          # to pass through arguments to home.nix
         };
 
       homeConfigurations."runner" = home-manager.lib.homeManagerConfiguration {
