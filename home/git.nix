a2ae8320ca6fa09c6a9ff249012088a9c632c4f2
Joshua Peek <josh@users.noreply.github.com>
2024-07-18 03:40:54 +0000

    Add gh

--- home/git.nix
+++ home/git.nix
@@ -1,10 +1,16 @@
 _: {
-  programs.git = {
-    enable = true;
-    userEmail = "josh@users.noreply.github.com";
-    userName = "Joshua Peek";
-    aliases = {
-      ci = "commit";
+  programs = {
+    git = {
+      enable = true;
+      userEmail = "josh@users.noreply.github.com";
+      userName = "Joshua Peek";
+      aliases = {
+        ci = "commit";
+      };
+    };
+
+    gh = {
+      enable = true;
     };
   };
 }
