fb1b5029bbff75871e6a559606f6924fc8bab43d
Joshua Peek <josh@users.noreply.github.com>
2024-07-18 07:19:16 +0000

    Configure some neovim plugins

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,8 +1,32 @@
-_: {
+{ pkgs, ... }:
+{
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = catppuccin-nvim;
+        config = "colorscheme catppuccin";
+      }
+      {
+        plugin = nvim-tree-lua;
+        config = ''
+          let g:loaded_netrw = 1
+          let g:loaded_netrwPlugin = 1
+          set termguicolors
+        '';
+      }
+      {
+        plugin = vim-startify;
+        config = "let g:startify_change_to_vcs_root = 0";
+      }
+    ];
+
+    extraConfig = '''';
+    extraLuaConfig = ''
+      require("nvim-tree").setup()
+    '';
   };
 }
