44d6e85adc4102c1f0103f821cfc2e5f40bf1cc5
Joshua Peek <josh@users.noreply.github.com>
2024-07-18 00:18:27 +0000

    More unix tools

--- home/utils.nix
+++ home/utils.nix
@@ -2,14 +2,22 @@
 {
   home.packages = with pkgs; [
     cowsay
+    du-dust
+    dua
+    duf
+    fd
     hello
     jq
     neofetch
     nixfmt-rfc-style
     nodePackages.prettier
+    rclone
+    restic
     ripgrep
+    rsync
     shellcheck
     shfmt
+    unzip
     wget
   ];
 }
