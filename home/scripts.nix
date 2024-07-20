9d8ecf0df6902d22c0c48dc11ebd3ac2888e682f
Joshua Peek <josh@users.noreply.github.com>
2024-07-20 07:05:38 +0000

    Use nh to perform home switch

--- home/scripts.nix
+++ home/scripts.nix
@@ -5,13 +5,14 @@ let
   '';
   hm-up = pkgs.writeShellScriptBin "hm-up" ''
     if [ -d .git ] && [ "$(${pkgs.git}/bin/git remote get-url origin)" = "https://github.com/josh/home-manager" ]; then
-        FLAKE_URI="$(pwd)"
+        FLAKE="$(pwd)"
     else
-        FLAKE_URI="github:josh/home-manager"
+        FLAKE="github:josh/home-manager"
     fi
-    echo "Using $FLAKE_URI as home manager flake" >&2
+    export FLAKE
+    echo "Using $FLAKE as home manager flake" >&2
 
-    exec ${pkgs.home-manager}/bin/home-manager switch --refresh --flake "$FLAKE_URI" -b backup
+    exec ${pkgs.nh}/bin/nh home switch --backup-extension backup
   '';
 in
 {
