5e6e7e1e904c202b31b3e00a86fd972953ca07c9
Joshua Peek <josh@users.noreply.github.com>
2024-07-21 09:49:13 +0000

    Rework inputs helper around ideas from flake-compat

--- inputs.nix
+++ inputs.nix
@@ -1,38 +1,64 @@
+# Helper to allow NixOS modules to access flake inputs.
+#
+# Cut down version of <https://github.com/edolstra/flake-compat> that only
+# resolves inputs skippping the root outputs.
+
 let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  fetchInput =
+  lockFile = builtins.fromJSON (builtins.readFile ./flake.lock);
+
+  fetchTree =
+    info:
+    assert info.type == "github";
     {
-      type,
-      owner,
-      repo,
-      rev,
-      narHash,
-      ...
-    }:
-    assert type == "github";
-    fetchTarball {
-      url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
-      sha256 = narHash;
+      outPath = fetchTarball ({
+        url = "https://github.com/${info.owner}/${info.repo}/archive/${info.rev}.tar.gz";
+        sha256 = info.narHash;
+      });
+      rev = info.rev;
+      shortRev = builtins.substring 0 7 info.rev;
+      lastModified = info.lastModified;
+      narHash = info.narHash;
     };
-  wrap =
-    pkg: buildOutputs:
+
+  resolveInput =
+    inputSpec: if builtins.isList inputSpec then getInputByPath "root" inputSpec else inputSpec;
+  getInputByPath =
+    nodeName: path:
+    if path == [ ] then
+      nodeName
+    else
+      getInputByPath (resolveInput lockFile.nodes.${nodeName}.inputs.${builtins.head path}) (
+        builtins.tail path
+      );
+
+  buildNode =
+    key: node:
+    assert key != "root";
     let
-      path = fetchInput pkg.locked;
-      outputs = buildOutputs path;
+      sourceInfo = fetchTree (node.info or { } // removeAttrs node.locked [ "dir" ]);
+      subdir = node.locked.dir or "";
+      outPath = sourceInfo + ((if subdir == "" then "" else "/") + subdir);
+      flake = import (outPath + "/flake.nix");
+      inputs = builtins.mapAttrs (inputName: inputSpec: nodes.${resolveInput inputSpec}) (
+        node.inputs or { }
+      );
+      outputs = flake.outputs (inputs // { self = result; });
+      result =
+        outputs
+        // sourceInfo
+        // {
+          inherit
+            outPath
+            inputs
+            outputs
+            sourceInfo
+            ;
+          _type = "flake";
+        };
+
     in
-    pkg // { inherit path outputs; };
+    result;
+  depNodes = builtins.removeAttrs lockFile.nodes [ "root" ];
+  nodes = builtins.mapAttrs buildNode depNodes;
 in
-{
-  nixpkgs = wrap lock.nodes.nixpkgs (path: {
-    default = import path;
-  });
-
-  home-manager = wrap lock.nodes.home-manager (path: {
-    default = import path;
-  });
-
-  catppuccin = wrap lock.nodes.catppuccin (path: {
-    homeManagerModules = import "${path}/modules/home-manager";
-    nixosModules = import "${path}/modules/nixos";
-  });
-}
+nodes
