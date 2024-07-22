d4736b5f0f396e42d4146fd0a28ab03e640a92ad
Joshua Peek <josh@users.noreply.github.com>
2024-07-22 08:11:18 +0000

    Enable vim style gitui bindings

--- home/git.nix
+++ home/git.nix
@@ -31,8 +31,16 @@ in
       # difftastic.enable = true;
     };
 
-    gitui.enable = true;
     gh.enable = true;
+
+    gitui = {
+      enable = true;
+      # Enable vim style key bindings.
+      keyConfig = builtins.fetchurl {
+        url = "https://raw.githubusercontent.com/extrawurst/gitui/0cb5b781a20eb742132bbcad923bcec1a744d8ec/vim_style_key_config.ron";
+        sha256 = "0mjz8c20qg0s4d5kv8y7wg0c9w42mjqj6fsgvib3g5cf404zv0mr";
+      };
+    };
   };
 
   home.packages = [
