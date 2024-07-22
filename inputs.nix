901aa13272a8781036c637b97bfa8191977d038e
Joshua Peek <josh@users.noreply.github.com>
2024-07-22 07:25:45 +0000

    Use inherit shorthand

--- inputs.nix
+++ inputs.nix
@@ -1,9 +1,7 @@
 let
   lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   node = lock.nodes.flake-compat.locked;
-  owner = node.owner;
-  repo = node.repo;
-  rev = node.rev;
+  inherit (node) owner repo rev;
   path = fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
     sha256 = node.narHash;
