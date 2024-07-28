a6440446377bdf7e81473d1e313a621f1980e25d
Joshua Peek <josh@users.noreply.github.com>
2024-07-28 09:24:53 +0000

    Try aligning config to macos dotfiles

--- home/neovim.nix
+++ home/neovim.nix
@@ -44,28 +44,57 @@ in
       };
 
       # File browser sidebar, open with <SPACE> + t
-      nvim-tree = {
+      neo-tree = {
         enable = true;
-        # Disable builtin file explorer
-        disableNetrw = true;
+        closeIfLastWindow = true;
       };
+
+      notify.enable = true;
+      noice = {
+        enable = true;
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+        };
+      };
+
+      which-key.enable = true;
 
       # Fuzzy file finder, open with <SPACE> + ff
       telescope = {
         enable = true;
         extensions.fzf-native.enable = true;
+        keymaps = {
+          "<leader>ff" = "find_files";
+          "<leader>fg" = "live_grep";
+          "<leader>b" = "buffers";
+          "<leader>fh" = "help_tags";
+          "<leader>fd" = "diagnostics";
+          "<C-p>" = "git_files";
+          "<leader>p" = "oldfiles";
+          "<C-f>" = "live_grep";
+        };
       };
 
       # git integration, open with <SPACE> + lg
       lazygit.enable = true;
 
-      # Try to figure out how treesitter and lsp interact
-      treesitter.enable = true;
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
+
       nix.enable = true;
+
       lsp = {
         enable = true;
         inlayHints = true;
         servers = {
+          lua-ls.enable = true;
           nixd.enable = true;
         };
       };
@@ -73,6 +102,51 @@ in
       # There's also coplilot-lua,
       # maybe try that and see what the difference is.
       copilot-vim.enable = true;
+
+      cmp = {
+        enable = true;
+        settings = {
+          sources = [ { name = "nvim_lsp"; } ];
+          mapping = {
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+          };
+        };
+        cmdline = {
+          "/" = {
+            sources = [ { name = "buffer"; } ];
+          };
+          ":" = {
+            sources = [
+              { name = "cmdline"; }
+              { name = "cmdline_history"; }
+              { name = "path"; }
+            ];
+          };
+        };
+      };
+
+      comment.enable = true;
+
+      conform-nvim = {
+        enable = true;
+        formattersByFt = {
+          json = [ "prettier" ];
+          lua = [ "stylua" ];
+          nix = [ "nixfmt" ];
+          python = [
+            "ruff_format"
+            "ruff_fix"
+          ];
+          yaml = [ "prettier" ];
+        };
+        formatOnSave = {
+          timeoutMs = 500;
+        };
+      };
     };
 
     # Change leader from \ to <SPACE>
@@ -80,19 +154,9 @@ in
 
     keymaps = [
       {
-        action = "<cmd>NvimTreeToggle<CR>";
+        action = "<cmd>Neotree action=focus reveal toggle<CR>";
         key = "<leader>t";
       }
-
-      {
-        action = "<cmd>Telescope live_grep<CR>";
-        key = "<leader>fw";
-      }
-      {
-        action = "<cmd>Telescope find_files<CR>";
-        key = "<leader>ff";
-      }
-
       {
         action = "<cmd>LazyGit<CR>";
         key = "<leader>lg";
