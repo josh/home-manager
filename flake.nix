d8415a582c58f8522f2b6d60cf5969d27cdf2365
Joshua Peek <josh@users.noreply.github.com>
2024-08-18 22:35:02 +0000

    Remove unused extra args

--- flake.nix
+++ flake.nix
@@ -63,7 +63,6 @@
       homeConfigurations = {
         "codespace" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs.username = "codespace";
           modules = [
             self.homeModules.default
             {
