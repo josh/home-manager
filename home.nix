8bbaa42945edb5db50fd468f5b79dc0411b165eb
Joshua Peek <josh@users.noreply.github.com>
2024-09-03 22:05:39 +0000

    Prefer SUDO_USER if possible

--- home.nix
+++ home.nix
@@ -4,9 +4,20 @@ let
 in
 {
   imports = lib.wrapImportsInputs lib.flakeInputs [ ./home ];
-  home.username = {
-    _type = "override";
-    content = builtins.getEnv "USER";
-    priority = 500;
-  };
+  home.username =
+    let
+      USER = builtins.getEnv "USER";
+      SUDO_USER = builtins.getEnv "SUDO_USER";
+    in
+    {
+      _type = "override";
+      content =
+        if SUDO_USER != "" then
+          SUDO_USER
+        else if USER != "" then
+          USER
+        else
+          null;
+      priority = 500;
+    };
 }
