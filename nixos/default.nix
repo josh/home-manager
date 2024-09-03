14b185adad1aeec2ae6f47ca0e7aa9bf34c14f62
Joshua Peek <josh@users.noreply.github.com>
2024-09-03 22:16:13 +0000

    Future todos

--- nixos/default.nix
+++ nixos/default.nix
@@ -6,5 +6,14 @@
 
   home-manager = {
     backupFileExtension = "backup";
+
+    sharedModules = {
+      # imports = [ ];
+    };
+
+    # TODO: Somehow allow default user to be set
+    # users.josh = {
+    #   imports = [ ];
+    # };
   };
 }
