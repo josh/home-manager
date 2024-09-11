b2247dc58d78a431501f40cba25a357f92d0a3fa
Joshua Peek <josh@users.noreply.github.com>
2024-09-11 17:34:30 +0000

    Skip update for now

--- home/nix.nix
+++ home/nix.nix
@@ -19,7 +19,7 @@ let
   os-up = pkgs.writeShellScriptBin "os-up" ''
     set -euo pipefail
 
-    ${pkgs.nh}/bin/nh os switch --out-link /tmp/os-up-result --update
+    ${pkgs.nh}/bin/nh os switch --out-link /tmp/os-up-result
     ${cachix-push} /tmp/hm-up-result
   '';
 
