49e6da430430c41192d0f4ce13e3c0639f0f469c
Joshua Peek <josh@users.noreply.github.com>
2024-09-14 22:39:59 -0700

    Add color warning

--- home/bin/cachix-push.sh
+++ home/bin/cachix-push.sh
@@ -3,6 +3,6 @@
 if [ -n "$CACHIX_AUTH_TOKEN" ] || [ -f "$HOME/.config/cachix/cachix.dhall" ]; then
   exec cachix push josh "$1"
 else
-  echo "cachix not configured, run cachix authtoken <TOKEN>" >&2
+  echo -e "\033[1;35mwarning:\033[0m cachix not configured, run cachix authtoken <TOKEN>" >&2
   exit 0
 fi
