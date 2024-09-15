ea59ae807490bcfef8fd928021882959659bbca0
Joshua Peek <josh@users.noreply.github.com>
2024-09-15 04:41:48 +0000

    Update flake

--- home/bin/os-up.sh
+++ home/bin/os-up.sh
@@ -10,6 +10,10 @@ fi
 # Ensure sudo wrapper is in PATH
 export PATH="/run/wrappers/bin:$PATH"
 
+pushd /etc/nixos
+nix flake update
+popd
+
 nh os switch --out-link /tmp/os-up-result /etc/nixos/
 cachix-push /tmp/os-up-result
 rm /tmp/os-up-result
