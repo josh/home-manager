b8d1d2a3b1f064e8f2e3a56ac71b73354c166002
Joshua Peek <josh@users.noreply.github.com>
2024-09-14 22:32:12 -0700

    Allow overriding home-manager input

--- home/bin/hm-up.sh
+++ home/bin/hm-up.sh
@@ -7,14 +7,16 @@ if [ -f /etc/NIXOS ]; then
   exit 1
 fi
 
-if [ -d .git ] && [ "$(git remote get-url origin)" = "https://github.com/josh/home-manager" ]; then
-  FLAKE="$(pwd)"
-else
-  FLAKE="github:josh/home-manager"
-fi
-export FLAKE
-echo "Using $FLAKE as home manager flake" >&2
+x() {
+  echo "+ $*" >&2
+  "$@"
+}
 
-nh home switch --backup-extension backup --out-link /tmp/hm-up-result -- --refresh
-cachix-push /tmp/hm-up-result
+FLAKE="github:josh/home-manager"
+if [ -d .git ] && [[ "$(git remote get-url origin)" == "https://github.com/josh/home-manager"* ]]; then
+  FLAKE="$(pwd)"
+fi
+
+x nh home switch "$FLAKE" --backup-extension backup --out-link /tmp/hm-up-result -- --refresh
+x cachix-push /tmp/hm-up-result
 rm /tmp/hm-up-result
--- home/bin/os-up.sh
+++ home/bin/os-up.sh
@@ -7,9 +7,19 @@ if [ ! -f /etc/NIXOS ]; then
   exit 1
 fi
 
+x() {
+  echo "+ $*" >&2
+  "$@"
+}
+
 # Ensure sudo wrapper is in PATH
 export PATH="/run/wrappers/bin:$PATH"
 
-nh os switch --update --out-link /tmp/os-up-result /etc/nixos/
-cachix-push /tmp/os-up-result
+if [ -d .git ] && [[ "$(git remote get-url origin)" == "https://github.com/josh/home-manager"* ]]; then
+  x nh os switch /etc/nixos --out-link /tmp/os-up-result -- --override-input josh-home-manager "$PWD"
+else
+  x nh os switch /etc/nixos --update --out-link /tmp/os-up-result
+fi
+
+x cachix-push /tmp/os-up-result
 rm /tmp/os-up-result
