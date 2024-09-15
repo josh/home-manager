744f75ea00803ef195d2090f1e984a8347020217
Joshua Peek <josh@users.noreply.github.com>
2024-09-15 04:33:02 +0000

    Add sudo wrapper to path

--- home/bin/os-up.sh
+++ home/bin/os-up.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+# Ensure sudo wrapper is in PATH
+export PATH="/run/wrappers/bin:$PATH"
+
 nh os switch --out-link /tmp/os-up-result /etc/nixos/
 cachix-push /tmp/os-up-result
