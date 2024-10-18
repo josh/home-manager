dea1fcd554b11ad50717bfdabec0b32c17ba9d4a
Joshua Peek <josh@users.noreply.github.com>
2024-10-18 15:40:21 +0000

    Add home-path pkg to hm path

--- home/home.nix
+++ home/home.nix
@@ -1,9 +1,15 @@
 # Configure user and home directory itself.
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   home = {
     username = lib.mkDefault "josh";
     homeDirectory =
       if "${config.home.username}" == "root" then "/root" else "/home/${config.home.username}";
   };
+  home.packages = [ pkgs.josh.home-path ];
 }
